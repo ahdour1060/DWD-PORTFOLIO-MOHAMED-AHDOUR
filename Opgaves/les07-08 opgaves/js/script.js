cc299f5116d34e5c63ce1f584dc22200552291c4
ahdour1060 <43809473+ahdour1060@users.noreply.github.com>
2020-04-05 22:51:38 +0200

    les07-08/V1

--- Opgaves/les07-08 opgaves/js/script.js
+++ Opgaves/les07-08 opgaves/js/script.js
@@ -1,4 +1,22 @@
 ;(function(){
     "use strict";
+
+    document.getElementById('btnSearch').addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        fetch('https://www.songsterr.com/a/ra/songs/byartists.json?artists=bono')
+        .then(function(resp) {
+            console.log('response status: ', resp.status);
+            return resp.json();
+        })
+        .then(function handleSuccess(data) {
+            console.log('data received: ', data);
+            alert(data.title);
+            
+        })
+        .catch(function(err) {
+            console.log('request failed: ', err)
+        });
+    });
     
 })();
