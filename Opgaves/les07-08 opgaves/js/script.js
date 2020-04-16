d8f1b2996f3265108e6110ddec775378865315c5
ahdour1060 <43809473+ahdour1060@users.noreply.github.com>
2020-04-16 17:50:43 +0200

    les07-08/V2

--- Opgaves/les07-08 opgaves/js/script.js
+++ Opgaves/les07-08 opgaves/js/script.js
@@ -4,19 +4,30 @@
     document.getElementById('btnSearch').addEventListener('click', (e) => {
         e.preventDefault();
         
-        fetch('https://www.songsterr.com/a/ra/songs/byartists.json?artists=bono')
+        let inpArtist = document.getElementById("inpArtist").value;
+        fetch('http://www.songsterr.com/a/ra/songs/byartists.json?artists="' + inpArtist + '"')
         .then(function(resp) {
             console.log('response status: ', resp.status);
             return resp.json();
         })
-        .then(function handleSuccess(data) {
+
+        .then(function (data) {
             console.log('data received: ', data);
-            alert(data.title);
-            
+
+            let songs = document.getElementById("songs");
+		    for(let i = 0; i < data.length; i++) {
+		    	let tr = document.createElement('tr');
+		    	tr.innerHTML = '<td>' + data[i].title + '</td>' + '<td>' + data[i].tabTypes + '</td>';
+                songs.appendChild(tr);
+            } 
+            let hidden = document.querySelector('.hidden');
+            hidden.style.opacity = 1;
         })
+
         .catch(function(err) {
             console.log('request failed: ', err)
-        });
+        })
+
+        
     });
-    
 })();
